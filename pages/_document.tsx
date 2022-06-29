91c89b378271cb55921dea2494ac32aab73a3e79
Alp Karavil <alp@hey.com>
2022-06-29 18:34:47 -0400

    add title and icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌊</text></svg>"
+        />
+      </Head>
       <body className="text-slate-900 bg-gradient-to-bl from-cyan-800 to-black min-h-screen">
         <Main />
         <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import Head from "next/head";
 import { DayToUserAssignment } from "../components/DayToUserAssignment";
 import { DayToUserAssignmentTable } from "../components/DayToUserAssignmentTable";
 import { Card } from "../components/ui/Card";
@@ -62,72 +63,77 @@ const Home = ({
   dayToUserNextWeek: Partial<Record<Day, User>>;
 }) => {
   return (
-    <div className="px-6 py-8 md:px-10 md:py-8 flex flex-col gap-6">
-      <h1 className="font-bold text-center sm:text-left text-[6vw] md:text-4xl text-white">
-        Welcome to Caladan.
-        <span className="mx-3">🌊 🦑</span>
-      </h1>
-      <div
-        className={cN(
-          "flex flex-row flex-wrap items-start justify-center gap-4"
-        )}
-      >
-        <div className="flex-[2]">
-          <Card
-            title="Laundry day assignments"
-            description="Weekends are first-come, first-serve. You have priority to do laundry on your assigned date; assignments are shuffled every Monday at 12 AM."
-          >
-            <div className="flex flex-col sm:flex-row justify-center items-center gap-4 md:gap-12">
-              <div className="flex-1/2 w-full">
-                <h1 className="font-bold">This week</h1>
-                <DayToUserAssignmentTable dayToUserMap={dayToUserThisWeek} />
-              </div>
-              <div className="flex-1/2 w-full">
-                <h1 className="font-bold">Next week</h1>
-                <DayToUserAssignmentTable dayToUserMap={dayToUserNextWeek} />
-              </div>
-            </div>
-          </Card>
-        </div>
-        <div className="flex-[1]">
-          <Card
-            title="Rent schedule"
-            description="Rent is due at the start of each month — don't be late!"
-          >
-            <a
-              rel="noreferrer"
-              target="_blank"
-              href="https://venmo.com/code?user_id=2307190153019392968&created=1656457085.845623&printed=1"
+    <>
+      <Head>
+        <title>House Atreides</title>
+      </Head>
+      <div className="px-6 py-8 md:px-10 md:py-8 flex flex-col gap-6">
+        <h1 className="font-bold text-center sm:text-left text-[6vw] md:text-4xl text-white">
+          Welcome to Caladan.
+          <span className="mx-3">🌊 🦑</span>
+        </h1>
+        <div
+          className={cN(
+            "flex flex-row flex-wrap items-start justify-center gap-4"
+          )}
+        >
+          <div className="flex-[2]">
+            <Card
+              title="Laundry day assignments"
+              description="Weekends are first-come, first-serve. You have priority to do laundry on your assigned date; assignments are shuffled every Monday at 12 AM."
             >
-              <div
-                className={cN(
-                  "flex flex-row justify-center items-center",
-                  "rounded-md overflow-hidden",
-                  "text-white h-8",
-                  "w-max"
-                )}
+              <div className="flex flex-col sm:flex-row justify-center items-center gap-4 md:gap-12">
+                <div className="flex-1/2 w-full">
+                  <h1 className="font-bold">This week</h1>
+                  <DayToUserAssignmentTable dayToUserMap={dayToUserThisWeek} />
+                </div>
+                <div className="flex-1/2 w-full">
+                  <h1 className="font-bold">Next week</h1>
+                  <DayToUserAssignmentTable dayToUserMap={dayToUserNextWeek} />
+                </div>
+              </div>
+            </Card>
+          </div>
+          <div className="flex-[1]">
+            <Card
+              title="Rent schedule"
+              description="Rent is due at the start of each month — don't be late!"
+            >
+              <a
+                rel="noreferrer"
+                target="_blank"
+                href="https://venmo.com/code?user_id=2307190153019392968&created=1656457085.845623&printed=1"
               >
-                <img
-                  alt="Venmo logo"
-                  className={cN("h-full py-2.5 px-4", "bg-blue-200 ")}
-                  src="/assets/venmo-logo.svg"
-                />
-                <span
+                <div
                   className={cN(
-                    "h-full flex justify-center items-center",
-                    "bg-blue-900 text-blue-100",
-                    "px-4",
-                    "font-mono text-sm"
+                    "flex flex-row justify-center items-center",
+                    "rounded-md overflow-hidden",
+                    "text-white h-8",
+                    "w-max"
                   )}
                 >
-                  @alpkv
-                </span>
-              </div>
-            </a>
-          </Card>
+                  <img
+                    alt="Venmo logo"
+                    className={cN("h-full py-2.5 px-4", "bg-blue-200 ")}
+                    src="/assets/venmo-logo.svg"
+                  />
+                  <span
+                    className={cN(
+                      "h-full flex justify-center items-center",
+                      "bg-blue-900 text-blue-100",
+                      "px-4",
+                      "font-mono text-sm"
+                    )}
+                  >
+                    @alpkv
+                  </span>
+                </div>
+              </a>
+            </Card>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
